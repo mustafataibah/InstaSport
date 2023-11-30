a0587fe3fec05d9d745cb6d631385fb1924623ca
andrewliu26 <aliu26@bu.edu>
2023-11-29 23:48:40 -0500

    updated LoginRegisterActivity frontend

--- app/src/main/java/com/amt/instasport/LoginRegisterActivity.kt
+++ app/src/main/java/com/amt/instasport/LoginRegisterActivity.kt
@@ -17,68 +17,88 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.compose.foundation.background
+
 
 @Composable
 fun LoginRegisterScreen(navController: NavController? = null) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val logoPainter = painterResource(id = R.drawable.logo)
-        Box(
-            modifier =
-            Modifier
-                .weight(1f)
-                .fillMaxWidth(),
-        ) {
-            Image(
-                painter = logoPainter,
-                contentDescription = "Logo",
-                modifier = Modifier.fillMaxSize(),
-            )
-        }
+        val logoPainter = painterResource(id = R.drawable.instasport_logo)
+        val typoPainter = painterResource(id = R.drawable.typography)
+
+        Spacer(modifier = Modifier.weight(2f))
+
+        Image(
+            painter = logoPainter,
+            contentDescription = "Logo",
+            modifier = Modifier.weight(1f)
+        )
+
+        Image(
+            painter = typoPainter,
+            contentDescription = "Typography",
+            modifier = Modifier
+                .fillMaxWidth(0.6f)
+                .weight(0.6f)
+        )
 
         // Buttons and clickable text
         Column(
-            modifier =
-            Modifier
-                .weight(1f)
-                .padding(16.dp),
+            modifier = Modifier.weight(2f),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Center
         ) {
-            Button(onClick = { navController?.navigate("register") }) {
-                Text("Sign Up with Email")
+            Button(
+                onClick = {
+                    navController?.navigate("register")
+                },
+                modifier = Modifier
+                    .width(220.dp)
+                    .height(50.dp)
+            ) {
+                Text("Sign Up with Phone")
             }
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(onClick = {}) {
+            Button(onClick = {},
+                modifier = Modifier
+                    .width(220.dp)
+                    .height(50.dp)
+            ) {
                 Text("Sign Up with Google")
             }
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(20.dp))
 
-            val annotatedText =
-                buildAnnotatedString {
-                    withStyle(style = SpanStyle(color = Color.White)) {
-                        append("Already a user? ")
-                    }
-                    pushStringAnnotation(tag = "LOG_IN", annotation = "login")
-                    withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline, color = Color.White)) {
-                        append("Log In")
-                    }
-                    pop()
+            val annotatedText = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color.Gray)) {
+                    append("Already a user? ")
                 }
+                pushStringAnnotation(tag = "LOG_IN", annotation = "login")
+                withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline, color = Color.Gray)) {
+                    append("Log In")
+                }
+                pop()
+            }
 
             ClickableText(
                 text = annotatedText,
                 onClick = { offset ->
                     annotatedText.getStringAnnotations(tag = "LOG_IN", start = offset, end = offset)
                         .firstOrNull()?.let { navController?.navigate("login") }
-                },
+                }
             )
         }
+
+        Spacer(modifier = Modifier.weight(2f))
+
     }
 }
 
