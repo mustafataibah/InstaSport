ad75340d6853409d3f6fa40f86b881d3c04a5b94
andrewliu26 <aliu26@bu.edu>
2023-12-14 12:42:26 -0500

    added icons and labels to navbar

--- app/src/main/java/com/amt/instasport/BottomNavBar.kt
+++ app/src/main/java/com/amt/instasport/BottomNavBar.kt
@@ -1,34 +1,63 @@
 package com.amt.instasport
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.List
 import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
 fun BottomNavBar(navController: NavController) {
+    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+
     NavigationBar {
         NavigationBarItem(
-            icon = { Icons.Outlined.Home },
-            label = { "Dashboard" },
-            selected = false,
-            onClick = { navController.navigate("dashboard") }
+            icon = {
+                Icon (imageVector = if (currentRoute == "dashboard") Icons.Filled.Home else Icons.Outlined.Home,
+                    contentDescription = "Dashboard")
+            },
+            label = { Text ("Dashboard") },
+            selected = currentRoute == "dashboard",
+            onClick = {
+                if (currentRoute != "dashboard") {
+                    navController.navigate("dashboard")
+                }
+            }
         )
         NavigationBarItem(
-            icon = { Icons.Outlined.List },
-            label = { "Events" },
-            selected = false,
-            onClick = { navController.navigate("events") }
+            icon = {
+                Icon (imageVector = if (currentRoute == "events") Icons.Filled.List else Icons.Outlined.List,
+                    contentDescription = "Events")
+            },
+            label = { Text ("Events") },
+            selected = currentRoute == "events",
+            onClick = {
+                if (currentRoute != "events") {
+                    navController.navigate("events")
+                }
+            }
         )
         NavigationBarItem(
-            icon = { Icons.Outlined.Person },
-            label = { "Profile" },
-            selected = false,
-            onClick = { navController.navigate("profile") }
+            icon = {
+                Icon (imageVector = if (currentRoute == "profile") Icons.Filled.Person else Icons.Outlined.Person,
+                    contentDescription = "Profile")
+            },
+            label = { Text ("Profile") },
+            selected = currentRoute == "profile",
+            onClick = {
+                if (currentRoute != "profile") {
+                    navController.navigate("profile")
+                }
+            }
         )
     }
 }
