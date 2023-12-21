b376eab5c6ab0a8d4119390a6eff49b294baa654
Mustafa <mustafataibah@gmail.com>
2023-12-20 20:05:34 -0500

    Events screen tabs and changed the onboarding graphic

--- app/src/main/java/com/amt/instasport/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/amt/instasport/ui/onboarding/OnboardingScreen.kt
@@ -1,25 +1,25 @@
 package com.amt.instasport.ui.onboarding
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
-import com.airbnb.lottie.compose.rememberLottieComposition
 import com.amt.instasport.R
 import com.amt.instasport.ui.component.ButtonsSection
 import com.amt.instasport.ui.component.PageIndicator
@@ -33,12 +33,11 @@ import com.google.accompanist.pager.rememberPagerState
 @Composable
 fun OnboardingScreen(navController: NavHostController) {
     val animations = listOf(
-        R.raw.animation, R.raw.animation, R.raw.animation
+        R.drawable.onboarding1, R.drawable.onboarding2, R.drawable.onboarding3
     )
     val titles = listOf(
         "Welcome to the Game", "Join the Action", "Host Your Game"
     )
-
     val descriptions = listOf(
         "Join our sports community and find your perfect game.",
         "Easily find and participate in local sports events.",
@@ -62,12 +61,13 @@ fun OnboardingScreen(navController: NavHostController) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(animations[currentPage]))
-                LottieAnimation(
-                    composition = composition,
-                    iterations = LottieConstants.IterateForever,
-                    modifier = Modifier.size(400.dp)
+                Image(
+                    painter = painterResource(id = animations[currentPage]),
+                    contentDescription = "User Avatar",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.Crop
                 )
+                Spacer(Modifier.height(40.dp))
                 Text(
                     text = titles[currentPage],
                     lineHeight = 40.sp,
--- app/src/main/java/com/amt/instasport/ui/view/EventsScreen.kt
+++ app/src/main/java/com/amt/instasport/ui/view/EventsScreen.kt
@@ -12,17 +12,25 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabPosition
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -41,43 +49,66 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.amt.instasport.R
 
-data class EventData (
+
+data class EventData(
     val title: String,
     val description: String,
     val author: String,
     val distance: Double,
 )
 
+enum class EventTab {
+    AllEvents, MySports
+}
+
 @Composable
 fun EventsScreen() {
-    // Test data
     val eventsList = listOf(
         EventData("Pickup Basketball", "Come play basketball with me!", "Jayson Tatum", 0.4),
         EventData("Tennis Match", "Let's play a few friendly sets!", "Serena Williams", 0.7),
         EventData(
-            "Volleyball Game",
-            "Come play volleyball with my friends and I!",
-            "Ricardo Souza",
-            1.2
+            "Volleyball Game", "Come play volleyball with my friends and I!", "Ricardo Souza", 1.2
         ),
         EventData(
-            "Recreational Soccer",
-            "Join us in a quick soccer game",
-            "Christiano Ronaldo",
-            1.3
+            "Recreational Soccer", "Join us in a quick soccer game", "Christiano Ronaldo", 1.3
         ),
     )
+
+    var selectedTab by remember { mutableStateOf(EventTab.AllEvents) }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(8.dp),
+            .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        TabRow(
+            selectedTabIndex = selectedTab.ordinal,
+            indicator = { tabPositions ->
+                CustomTabIndicator(tabPositions[selectedTab.ordinal])
+            },
+            divider = { },
+            containerColor = Color.Transparent,
+            modifier = Modifier.padding(vertical = 8.dp)
+        ) {
+            Tab(
+                selected = selectedTab == EventTab.AllEvents,
+                onClick = { selectedTab = EventTab.AllEvents },
+                text = { Text("All Events") },
+                selectedContentColor = MaterialTheme.colorScheme.onSurface,
+                unselectedContentColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+            )
+            Tab(
+                selected = selectedTab == EventTab.MySports,
+                onClick = { selectedTab = EventTab.MySports },
+                text = { Text("My Sports") },
+                selectedContentColor = MaterialTheme.colorScheme.onSurface,
+                unselectedContentColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+            )
+        }
         LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
+            modifier = Modifier.fillMaxSize()
         ) {
             items(eventsList) { event ->
                 EventItem(event)
@@ -87,29 +118,43 @@ fun EventsScreen() {
 }
 
 @Composable
+fun CustomTabIndicator(tabPosition: TabPosition) {
+    val indicatorWidth = tabPosition.width * 0.8f
+    val indicatorOffset = tabPosition.left + (tabPosition.width - indicatorWidth) / 2
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.BottomStart)
+            .offset(x = indicatorOffset)
+            .width(indicatorWidth)
+            .height(3.dp)
+            .background(
+                color = MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(1.5.dp)
+            )
+    )
+}
+
+@Composable
 fun EventItem(event: EventData) {
     val (title, description, author, distance) = event
     var isExpanded by remember { mutableStateOf(false) }
 
-    Card(
-        colors = CardDefaults.cardColors(
-            containerColor =
-                if (isExpanded) MaterialTheme.colorScheme.secondaryContainer else Color.White
-        ),
+    Card(colors = CardDefaults.cardColors(
+        containerColor = if (isExpanded) MaterialTheme.colorScheme.secondaryContainer else Color.White
+    ),
         border = if (isExpanded) null else BorderStroke(1.dp, Color.LightGray),
         modifier = Modifier
-            .padding(8.dp)
             .fillMaxWidth()
-            .clickable { isExpanded = !isExpanded }
-    ) {
+            .padding(vertical = 8.dp)
+            .clickable { isExpanded = !isExpanded }) {
         Column(
             modifier = Modifier
                 .padding(16.dp)
                 .animateContentSize(),
         ) {
-            Row (
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
+            Row(
+                modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically
             ) {
                 Box(
                     modifier = Modifier
@@ -130,8 +175,7 @@ fun EventItem(event: EventData) {
                 )
                 Spacer(modifier = Modifier.weight(1f))
                 Text(
-                    text = "$distance mi away",
-                    modifier = Modifier.padding(horizontal = 4.dp)
+                    text = "$distance mi away", modifier = Modifier.padding(horizontal = 4.dp)
                 )
             }
 
@@ -149,8 +193,7 @@ fun EventItem(event: EventData) {
 
             if (isExpanded) {
                 Text(
-                    text = description,
-                    modifier = Modifier.fillMaxWidth()
+                    text = description, modifier = Modifier.fillMaxWidth()
                 )
                 Spacer(modifier = Modifier.padding(6.dp))
 
@@ -160,8 +203,7 @@ fun EventItem(event: EventData) {
                     verticalAlignment = Alignment.CenterVertically,
                 ) {
                     OutlinedButton(
-                        onClick = { /*TODO*/ },
-                        modifier = Modifier.padding(
+                        onClick = { /*TODO*/ }, modifier = Modifier.padding(
                         )
                     ) {
                         Text(
@@ -170,8 +212,7 @@ fun EventItem(event: EventData) {
                     }
                     Spacer(modifier = Modifier.padding(4.dp))
                     Button(
-                        onClick = { /*TODO*/ },
-                        modifier = Modifier.padding(
+                        onClick = { /*TODO*/ }, modifier = Modifier.padding(
                         )
                     ) {
                         Text(
